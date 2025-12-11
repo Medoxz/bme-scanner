97a5c0ac090721981210105636fd54713aa4c100
Joris van Roekel <j.w.vanroekel@student.tudelft.nl>
2025-12-11 02:21:07 +0100

    Removed scan product button from home page

--- flutter-app/lib/pages/home_page.dart
+++ flutter-app/lib/pages/home_page.dart
@@ -12,28 +12,6 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.camera_alt_rounded,
-                size: 28,
-                color: Colors.white,
-              ),
-              label: Text(
-                'Scan Product',
-                style: const TextStyle(fontSize: 20, color: Colors.white),
-              ),
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 60),
-                padding: const EdgeInsets.all(8),
-                backgroundColor: const Color(0xFF4B986C),
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
             Text(
               "Welcome to the Ingredient Scanner!",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
